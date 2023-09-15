33b2217cbe7aa3089b1a82cb1462215b9398d2b6
amirthenew <ah.abshari@hotmail.com>
2023-09-15 21:10:00 +0430

    tailwind instaled

---
+++ webidemysite/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+
+    colors:{
+    'darkcolor' : '#212121',
+    'webidemy' : '#fea300'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
