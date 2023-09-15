9f8ada5a6686ed2b8aa36434cc5566e40dc95840
amirthenew <ah.abshari@hotmail.com>
2023-09-15 21:16:32 +0430

    first step

--- webidemysite/src/App.jsx
+++ webidemysite/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-<h1 className='text-3xl text-webidemy font-bold '>webidemy</h1>
-      </div>
+<div>
 
-    </>
+
+      <Header/>
+<h1 className='text-3xl text-webidemy font-bold '>webidemy</h1>
+
+
+    </div>
   )
 }
 
---
+++ webidemysite/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+
+        <div className='w-full h-40 bg-darkcolor'> 
+        </div>
+      );
+}
+ 
+export default Header;
