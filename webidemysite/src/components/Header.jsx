5ae982ba9f9015426d5a683661d85940f0c30b89
amirthenew <ah.abshari@hotmail.com>
2023-09-15 21:37:43 +0430

    now we have logo

--- webidemysite/src/components/Header.jsx
+++ webidemysite/src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
 
-        <div className='w-full h-40 bg-darkcolor'> 
+        <div className='flex w-full h-40 bg-darkcolor'> 
+        <div>
+        <img className='w-20 m-3' src='../../src/assets/images/WEBIDEMI.png'/>
+        </div>
         </div>
       );
 }
